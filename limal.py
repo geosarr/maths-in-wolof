a330453db8367e792ba4c4c8006cc0a6aeda1a32
geosarr <83412621+geosarr@users.noreply.github.com>
2023-12-19 10:01:19 +0100

    Jeemantu (#2)
    * Initial commit.
    
    * Yoku lim ak python.
    
    * Yok test lim ba milliong.
    
    * Yoku na lim ba milliong.
    
    * Yoku woral limukay bi ci milliard yi.
    
    * Yok na lim yi ba milliong.
    
    * Lim yi ba téeméer-junni ngir wañi dissayu llim_yi.txt.
    
    ---------
    
    Co-authored-by: Georges SARR <georges.sarr@sia-partners.com>

---
+++ limal.py
@@ -0,0 +1,77 @@
+import click
+
+# Lim yu jitu ci been been yi, fukk fukk yi,
+# téeméer téemer yi, juuni juuni yi, ...
+LIM = {
+    0: "tus",
+    1: "been",
+    2: "ñaar",
+    3: "ñatt",
+    4: "ñeent",
+    5: "juròom",
+    10: "fukk",
+    30: "fanweer",
+    100: "téeméer",
+    1000: "junni",
+    int(1e6): "milliong",
+    int(1e9): "milliard",
+}
+
+
+def fukk_fukku(n: int) -> str:
+    return _limal(n, 10, [1, 3])
+
+
+def teemeer_teemeeru(n) -> str:
+    return _limal(n, 100)
+
+
+def junni_junniu(n: int) -> str:
+    return _limal(n, 1000)
+
+
+def milliong_milliongu(n: int) -> str:
+    return _limal(n, int(1e6))
+
+
+def milliar_milliaru(n: int) -> str:
+    return _limal(n, int(1e9))
+
+
+def _limal(n: int, d: int, wute: list[int] = [1]) -> str:
+    q, r = n // d, n % d
+    lim = f"{limal(q)}-{limal(d)}" if q not in wute else limal(q * d)
+    return f"{lim}-ak-{limal(r)}" if r > 0 else lim
+
+
+def limal(n: int) -> str:
+    if not isinstance(n, int):
+        raise NotImplementedError("n lim bu mët la wara done.")
+    if n < 0:
+        raise NotImplementedError("n dafa wara >= 0.")
+    if n in LIM:
+        return LIM[n]
+    elif n < 10:
+        return f"{LIM[5]}-{LIM[n%5]}"
+    elif n < 100:
+        return fukk_fukku(n)
+    elif n < 1000:
+        return teemeer_teemeeru(n)
+    elif n < 1e6:
+        return junni_junniu(n)
+    elif n < 1e9:
+        return milliong_milliongu(n)
+    elif n < 1e12:
+        return milliar_milliaru(n)
+    raise NotImplementedError
+
+
+@click.command()
+@click.option("--lim", "-l", required=True, type=int)
+def main(lim: int):
+    for a in range(lim):
+        print(a, ":", limal(a))
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,37 @@
+import unittest
+from limal import *
+
+
+class TestLimal(unittest.TestCase):
+    def test_limal(self):
+        self.assertEqual(limal(23), "ñaar-fukk-ak-ñatt")
+        self.assertEqual(limal(32), "fanweer-ak-ñaar")
+        self.assertEqual(limal(39), "fanweer-ak-juròom-ñeent")
+        self.assertEqual(limal(81), "juròom-ñatt-fukk-ak-been")
+        self.assertEqual(limal(131), "téeméer-ak-fanweer-ak-been")
+        self.assertEqual(limal(264), "ñaar-téeméer-ak-juròom-been-fukk-ak-ñeent")
+        self.assertEqual(limal(618), "juròom-been-téeméer-ak-fukk-ak-juròom-ñatt")
+        self.assertEqual(limal(1073), "junni-ak-juròom-ñaar-fukk-ak-ñatt")
+        self.assertEqual(limal(100000), "téeméer-junni")
+        self.assertEqual(
+            limal(20456),
+            "ñaar-fukk-junni-ak-ñeent-téeméer-ak-juròom-fukk-ak-juròom-been",
+        )
+        self.assertEqual(
+            limal(987000),
+            "juròom-ñeent-téeméer-ak-juròom-ñatt-fukk-ak-juròom-ñaar-junni",
+        )
+        self.assertEqual(limal(1000006), "milliong-ak-juròom-been")
+        self.assertEqual(
+            limal(283496736),
+            "ñaar-téeméer-ak-juròom-ñatt-fukk-ak-ñatt-milliong-ak-ñeent-téeméer-ak-juròom-ñeent-fukk-ak-juròom-been-junni-ak-juròom-ñaar-téeméer-ak-fanweer-ak-juròom-been",
+        )
+        self.assertEqual(limal(int(1e9)), "milliard")
+        self.assertEqual(
+            limal(103010038510),
+            "téeméer-ak-ñatt-milliard-ak-fukk-milliong-ak-fanweer-ak-juròom-ñatt-junni-ak-juròom-téeméer-ak-fukk",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
